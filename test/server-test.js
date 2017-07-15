5c1686546952bf2e629b00c76a90983f67785f8d
Placid Rodrigues <placidrod@gmail.com>
2017-07-14 18:07:48 -0700

    Add failing test

--- test/server-test.js
+++ test/server-test.js
@@ -2,6 +2,6 @@ var expect = require('chai').expect;
 
 describe('test test', function() {
   it('test description', function() {
-    expect(true).to.equal(true);
+    expect(true).to.equal(false);
   });
 });
